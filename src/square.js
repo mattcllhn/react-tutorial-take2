2fd178c59afaba5c05f25b6953538d2a9a1094d1
matt <mattcllhn@gmail.com>
2018-10-04 19:44:54 -0500

    lifted state to board component

--- src/square.js
+++ src/square.js
@@ -2,10 +2,14 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import './index.css';
 export default class Square extends React.Component {
+
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button 
+                className="square" 
+                onClick = {this.props.onClick} 
+                >
+                {this.props.value}
             </button>
         );
     }
